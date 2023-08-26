83c1fcf712f7981eb65364f3923034bc282f8fe6
Weki Work labs <ukonuzoidx@gmail.com>
2023-08-26 23:55:34 +0100

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.post('/flutterwave-webhook', (req, res) => {
-  const eventData = req.body;
-  // Process the event data here
-  console.log(eventData);
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
 
-  // Respond to Flutterwave
+app.post("/flutterwave-webhook", (req, res) => {
+  const eventData = req.body;
+
+  // Assuming you're interested in the 'charge.completed' event
+  if (eventData.event === "charge.completed") {
+    console.log("Charge completed event received:", eventData);
+
+    // Additional processing here. For instance, store in a database, etc.
+  } else {
+    console.log("Other event received:", eventData);
+  }
+
+  // Respond to Flutterwave to acknowledge receipt
   res.sendStatus(200);
 });
 
-
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
